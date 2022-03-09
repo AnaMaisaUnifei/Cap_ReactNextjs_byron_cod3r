17d578555775c85579dad5e9477e9f82bb2efc4b
Ana Maísa <d2021002575@unifei.edu.br>
2022-03-09 15:01:37 -0300

    Primeiros componentes

---
+++ exercicios/pages/basicos/primeiro.js
@@ -0,0 +1,4 @@
+// Sempre que se cria um arquivo dentro de pages, ele cria um caminho pela URL. É necessário exportar como default os componentes para que eles possam ser renderizados
+export default function primeiro() {
+  return "Primeiro"
+}
