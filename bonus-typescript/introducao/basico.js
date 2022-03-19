658a96ea8865a9fe123f8394640dd600c8e1e2c9
Ana Maísa <d2021002575@unifei.edu.br>
2022-03-18 22:08:01 -0300

    Formas de executar TS

---
+++ bonus-typescript/introducao/basico.js
@@ -0,0 +1,4 @@
+"use strict";
+// Compilação manual por linha de comando = tsc <nomeDoArq>; depois, ele vai criar um arquivo JS e é só executar esse JS com node = node <nomeDoArq>
+const a = 'Teste TS aaa';
+console.log(a);
---
+++ bonus-typescript/introducao/basico.ts
@@ -0,0 +1,3 @@
+// Compilação manual por linha de comando = tsc <nomeDoArq>; depois, ele vai criar um arquivo JS e é só executar esse JS com node = node <nomeDoArq>
+const a: string = 'Teste TS aaa'
+console.log(a)
