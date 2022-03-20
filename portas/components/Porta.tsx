7d6266f6ee4a2cc1d43c9cf164686baf6b972685
Ana Maísa <d2021002575@unifei.edu.br>
2022-03-19 23:29:17 -0300

    Configurando a criação e os comportamentos das portas

--- portas/components/Porta.tsx
+++ portas/components/Porta.tsx
@@ -1,21 +1,38 @@
 import styles from "../styles/Porta.module.css"
 import PortaModel from '../model/porta'
+import Presente from './Presente'
 
 interface PortaProps {
-  porta: PortaModel
+  value: PortaModel
+  onChange: (novaPorta: PortaModel) => void
 }
 
 export default function Porta(props: PortaProps) {
-  const { porta } = props
-  const selecioanda = porta.selecionada ? styles.selecionada : ''
+  const porta = props.value
+  const selecioanda = porta.selecionada && !porta.aberta ? styles.selecionada : ''
 
-  return (
-    <div className={styles.area}>
-      <div className={`${styles.batente} ${selecioanda}`}>
-        <div className={styles.porta}>
+  const alternarSelecao = e => props.onChange(porta.alternarSelecao())
+  const abrir = e => {
+    e.stopPropagation()
+    props.onChange(porta.abrir())
+  }
+
+  function renderizarPorta() {
+    return (
+      <div className={styles.porta}>
           <div className={styles.numero}>{porta.numero}</div>
-          <div className={styles.macaneta}></div>
+          <div className={styles.macaneta} 
+            onClick={abrir} ></div>
         </div>
+    )
+  }
+  
+  return (
+    <div className={styles.area} onClick={alternarSelecao}>
+      <div className={`${styles.batente} ${selecioanda}`}>
+        {porta.fechada ? 
+        renderizarPorta() : 
+        porta.temPresente ? <Presente /> : false }
       </div>
       <div className={styles.chao}></div>
     </div>
