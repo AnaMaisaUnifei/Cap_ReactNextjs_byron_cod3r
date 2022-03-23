f8b22960fd5494ec1ac0686191e62c4ffdc7c0ad
Ana Maísa <d2021002575@unifei.edu.br>
2022-03-23 00:30:49 -0300

    Usando Fallback True

--- renderizacao/pages/alunos/[id].jsx
+++ renderizacao/pages/alunos/[id].jsx
@@ -7,12 +7,13 @@ export async function getStaticPaths() {
   })
 
   return {
-    fallback: false, // 404 quando não encontrar um id que está nos parâmetros
+    fallback: true, // false = 404 -> quando não encontrar um id que está nos parâmetros
     paths
   }
 }
 
 export async function getStaticProps({params}) {
+  console.log(params)
   const resp = await fetch(`http://localhost:3000/api/alunos/${params.id}`)
   const aluno = await resp.json()
 
@@ -29,11 +30,14 @@ export default function AlunoPorId(props) {
   return (
     <div>
       <h1>Detalhes do Aluno</h1>
-      <ul>
-        <li>{aluno.id}</li>
-        <li>{aluno.nome}</li>
-        <li>{aluno.email}</li>
-      </ul>
+      {aluno ?
+        <ul>
+          <li>{aluno.id}</li>
+          <li>{aluno.nome}</li>
+          <li>{aluno.email}</li>
+        </ul>
+        : false
+      }
     </div>
   )
 }
